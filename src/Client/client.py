267c7bcc904cb13f47ec28fa94096a8464911223
Lora K <lora.kalthoff@gmail.com>
2020-11-20 14:23:18 -0700

    Basic Client and added client_app

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,2 +1,25 @@
+import socket
+
+
 class Client:
-    pass
+    def __init__(self, ip: str, port: int):
+        self.__ip = ip
+        self.__port = port
+        self.__received_msgs = []
+        self.__server_receive = None
+        self.__is_connected = False
+
+    def connect(self):
+        pass
+
+    def send_message(self, msg: str):
+        pass
+
+    def receive_message(self):
+        pass
+
+    def print_messages(self):
+        pass
+
+    def disconnect(self):
+        pass
