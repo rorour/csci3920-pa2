0d65c07b29db048b2ea540378501fb4bd207b1e2
rorour <ravenmarieorourke@gmail.com>
2020-11-20 13:21:35 -0700

    created client & server files

---
+++ Server/server.py
@@ -0,0 +1,17 @@
+class Server:
+    def __init__(self):
+        self.__queued_messages = []
+        self.__connected_clients = []
+        self.__user_list = []
+
+    def load_from_file(self):
+        pass
+
+    def save_data(self):
+        pass
+
+    def start_service(self):
+        pass
+
+    def stop_service(self):
+        pass
